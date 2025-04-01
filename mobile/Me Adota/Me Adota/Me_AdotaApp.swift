215f6fdf069864f3530a7ad8a5e05303ca41d0ec
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2025-04-01 18:31:14 -0300

    add xcode project

---
+++ mobile/Me Adota/Me Adota/Me_AdotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Me_AdotaApp.swift
+//  Me Adota
+//
+//  Created by Anderson Sprenger on 01/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Me_AdotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
