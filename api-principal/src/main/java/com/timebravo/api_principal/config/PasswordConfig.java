0d60d3ba70b62dbdd007a0a1be5610035f9137ae
romulopitt <romulo.pitt@edu.pucrs.br>
2025-06-17 04:47:14 -0300

    ajustes data.sql, senha texto (1) para teste

--- api-principal/src/main/java/com/timebravo/api_principal/config/PasswordConfig.java
+++ api-principal/src/main/java/com/timebravo/api_principal/config/PasswordConfig.java
@@ -3,12 +3,14 @@ package com.timebravo.api_principal.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class PasswordConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        //return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance(); //para testes
     }
 }
