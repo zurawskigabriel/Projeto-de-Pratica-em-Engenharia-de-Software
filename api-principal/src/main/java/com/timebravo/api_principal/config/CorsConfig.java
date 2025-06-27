151b9902954bedef30b32fedaf607f54b3ec6a7f
erik-monteiroo <erikmonteiroribeiropc@hotmail.com>
2025-06-27 15:34:04 -0300

    testando subir aplicação com DOCKER - problema de CORS, tentando resolver

--- api-principal/src/main/java/com/timebravo/api_principal/config/CorsConfig.java
+++ api-principal/src/main/java/com/timebravo/api_principal/config/CorsConfig.java
@@ -8,15 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
+       public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedOrigins(
+                            "http://localhost:8081",         // Quando roda fora do Docker
+                            "http://frontend:8081",          // Quando frontend acessa a API por nome do serviço
+                            "http://host.docker.internal:8081" // Quando acessado por IP do host
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
